ff26f71ef2d44d0f9e7d99ecfd750d0c55732e13
nauLvero <naulvero@gmail.com>
2023-01-21 14:12:13 +0100

    Program using iterator

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void showVector(vector<int> p_vector)
+{
+    auto itVec = p_vector.begin(); // auto - zastepczy typ zmiennej begin()zwraca pierwszy iterator
+    for (itVec; itVec != p_vector.end(); itVec++)
+    {
+        cout << "|" << *itVec;
+    };
+    cout << "|" << endl;
+};
+
 int main()
 {
+    vector<int> vector1{2, 3, 4, 5, 6, 18};
+    vector<int> vector2{1, 5, 7, 3, 5, 3, 21, 31};
+    showVector(vector1);
+    showVector(vector2);
     return 0;
 };
